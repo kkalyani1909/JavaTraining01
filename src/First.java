694c3b5bf223cec2e563216df3695ef690e9c91a
Kalyani <Kalyani@DESKTOP-ATVHHCR>
2022-10-11 13:17:36 +0530

    My first commit

---
+++ src/First.java
@@ -0,0 +1,43 @@
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Numbers and Tables");
+		System.out.println("2+3" + "=" + (2+3));
+		//sum of 2+3=5
+		System.out.println("Hello Java");
+		//to get the result as it is 
+		
+		System.out.println("2+5");
+		//to get the result as it is in the inverted quotes
+		
+		System.out.println("5+9" + "=" + (5+9));
+		//to get the concatenation script as a sum result
+		
+		System.out.println("9-6" + "=" + (9-6));
+		// to get the concatenation script as a subtract result
+		
+		System.out.println(9-6);
+		// to get direct result of the subtraction question
+		
+		System.out.println(5*2);
+		// to get direct result of the multiplication question
+		
+		System.out.println("9/3" + "=" + (9/3));
+		// to get concatenation result
+		
+		System.out.print("First script test in java.");
+		System.out.print("That is for addition, subtract, multiplicaton etc.");
+		System.out.println("Learnt with strings and integers.");
+		// used here print without ln statement to get the result in one line.
+		
+		System.out.println(9*3);
+		
+		
+		
+
+	}
+
+}
